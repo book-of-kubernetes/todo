a4af4e8e439d47fac9f92150aca6459dce4278b4
Alan Hohn <alanhohn@gmail.com>
2022-04-03 13:52:16 -0400

    fix metrics

--- server.js
+++ server.js
@@ -10,6 +10,16 @@ console.log(`Todo service listening on port ${port} with prefix ${prefix}`);
 
 var router = express.Router();
 
+// ----- Metrics counters
+const success = new prom.Counter({ name: 'api_success', help: 'Successful responses' });
+const failure = new prom.Counter({ name: 'api_failure', help: 'Failed responses' });
+const getAll = new prom.Counter({ name: 'api_retrieve_all', help: 'Retrieve all items' });
+const getOne = new prom.Counter({ name: 'api_retrieve_id', help: 'Retrieve single item' });
+const create = new prom.Counter({ name: 'api_create', help: 'Create item' });
+const update = new prom.Counter({ name: 'api_update', help: 'Update item' });
+const remove = new prom.Counter({ name: 'api_remove', help: 'Remove item' });
+const clear = new prom.Counter({ name: 'api_clear', help: 'Clear items' });
+
 // ----- The API implementation
 
 var backendConfig = {};
@@ -22,10 +32,12 @@ var todos = backend(backendConfig);
 function createCallback(res, onSuccess) {
   return function callback(err, data) {
     if (err || !data) {
+      failure.inc();
       res.status(500).send('Something bad happened!');
       return;
     }
 
+    success.inc();
     onSuccess(data);
   }
 }
@@ -47,36 +59,42 @@ function getCreateTodo(req) {
 }
 
 router.get('/api/', function(req, res) {
+  getAll.inc();
   todos.all(createCallback(res, function(todos) {
     res.send(todos.map(getCreateTodo(req)));
   }));
 });
 
 router.get('/api/:id', function(req, res) {
+  getOne.inc();
   todos.get(req.params.id, createCallback(res, function(todo) {
     res.send(createTodo(req, todo));
   }));
 });
 
 router.post('/api', function(req, res) {
+  create.inc();
   todos.create(req.body.title, req.body.order, createCallback(res, function(todo) {
     res.send(createTodo(req, todo));
   }));
 });
 
 router.patch('/api/:id', function(req, res) {
+  update.inc();
   todos.update(req.params.id, req.body, createCallback(res, function(todo) {
     res.send(createTodo(req, todo));
   }));
 });
 
 router.delete('/api/', function(req, res) {
+  remove.inc();
   todos.clear(createCallback(res, function(todos) {
     res.send(todos.map(getCreateTodo(req)));
   }));
 });
 
 router.delete('/api/:id', function(req, res) {
+  clear.inc();
   todos.delete(req.params.id, createCallback(res, function(todo) {
     res.send(createTodo(req, todo));
   }));
@@ -84,10 +102,20 @@ router.delete('/api/:id', function(req, res) {
 
 // ----- Prometheus metrics
 
+prom.register.setDefaultLabels({
+  app: 'todo'
+})
+
 prom.collectDefaultMetrics();
 router.get('/metrics', (req, res) => {
-	res.set('Content-Type', prom.register.contentType);
-	res.end(prom.register.metrics());
+  prom.register.metrics().then(function(metrics) {
+    res.set('Content-Type', prom.register.contentType);
+    res.send(metrics);
+    res.end();
+  }).catch(function(err) {
+    res.status(500).send('Something bad happened!');
+    return;
+  });
 });
 
 var app = express();
